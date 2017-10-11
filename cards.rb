9cf4a560438bacf810ea66e2af9119c859795a47
Abby Christens <achristens@gmail.com>
2017-10-11 13:19:03 -0400

    Only allow user to pick up 3 cards

--- cards.rb
+++ cards.rb
@@ -1,16 +1,15 @@
 require 'pry'
 def select_cards(possible_cards, hand)
-
   possible_cards.each do |current_card|
-    print "Do you want to pick up #{current_card}?"
-    answer = gets.chomp
-    if answer.downcase == 'y'
-      hand << current_card
+    if hand.length < 3
+      print "Do you want to pick up #{current_card}?"
+      answer = gets.chomp
+      if answer.downcase == 'y'
+        hand << current_card
+      end
     end
   end
   hand
-  # binding.pry
-
 end
 
 available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
